490dadb278a7e9ced74828f6da7d915c7cc2c69d
RazaRocket <33154105+RazaRocket@users.noreply.github.com>
2020-02-09 19:03:39 +0100

    Add files via upload
    scrapping object from messier catalog

---
+++ src/scrapping.py
@@ -0,0 +1,37 @@
+# Importer les librairies pour le scrapping
+import requests as rq
+from bs4 import BeautifulSoup as bs
+
+# Charger la page que l'on veyt scrapper
+r = rq.get('https://fr.wikibooks.org/wiki/Mini-guide_du_catalogue_Messier/Galaxies')
+
+soup = bs(r.content, 'html5lib')
+
+tables = soup.find_all('table', class_='noprint')
+
+# Lister les éléments dans un tableau
+data = []
+for table in tables:
+    trs = table.find_all('tr')
+    row = dict()
+    for tr in trs:
+        try:
+            row[tr.td.get_text().strip()] = tr.td.next_sibling.next_sibling.get_text().strip()
+        except AttributeError:
+            pass
+    data.append(row)
+
+    row = dict()
+
+    trs = soup.find('b', text='Type').parent.parent.parent.find_all('tr')
+
+
+    for tr in trs:
+    try:
+        row[tr.td.get_text().strip()] = tr.td.next_sibling.next_sibling.get_text().strip()
+    except AttributeError:
+        pass
+
+
+    srcs = [ img['image'] for img in soup.select('.picCore[image]')]
+print(srcs)
